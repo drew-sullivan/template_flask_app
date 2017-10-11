f9eea7a277403b8448d99a7654a0f6f8e90b6df9
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-10-11 16:41:38 -0400

    Added profile change form

--- app/main/views.py
+++ app/main/views.py
@@ -1,31 +1,15 @@
 from flask import render_template, redirect, url_for, abort, flash
 from flask_login import login_required, current_user
 from . import main
-from .forms import EditProfileForm, EditProfileAdminForm
+from .forms import EditProfileForm
 from .. import db
 from ..models import Role, User
 from ..decorators import admin_required
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['CLEVELAND_TECH_SURVEY_ADMIN']:
-                send_email(current_app.config['CLEVELAND_TECH_SURVEY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    return render_template('index.html')
 
 
 @main.route('/user/<username>')
